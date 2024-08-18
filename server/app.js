9c4453e3597ed4f6fb918a7139cb9be9bd6d2131
Jhonattan Gonzalez <63214951+JHONATTANG@users.noreply.github.com>
2024-08-18 12:44:00 -0500

    correction

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const puerto = 3050;
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/home', express.static(path.join(__dirname, '/public')));
 
 app.use((req,res) => {
     res.status(404);
