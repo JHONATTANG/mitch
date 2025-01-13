ff4aa92b92482a49b71f4f2ec2216377512ab64f
Jhntt <jhonattan.gonzalez.38@gmail.com>
2025-01-12 21:28:50 -0500

    responsive

--- server/app.js
+++ server/app.js
@@ -5,14 +5,51 @@ const app = express();
 
 const puerto = 3050;
 
+// archivos estaticos desde public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ruta principal
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
+})
+
+// ruta para cambiar contenido SECTION
+app.get('/section/:name', (req,res) => {
+    const sectionName = req.params.name;
+    const filePath = path.join(__dirname,'public', 'sections',`${sectionName}.html`);
+
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            res.status(404).send('<p>Sección no encontrada.</p>');
+        }
+    });
+})
+
+// puerto
+app.listen(puerto,()=>{
+    console.log("App listen port "+puerto);
+})
+
+
+//original
+/*
 app.use('/', express.static(path.join(__dirname, '/public')));
+
+
+
+
+
 
 app.use((req,res) => {
     res.status(404);
-    res.send(`<h1>ERROR 404: Page not found</h1><p>Go to the page <a href="https://mitchdigital.online/">Home</a></p>`)
+    res.send(`<h1>ERROR 404: Page not found</h1><p>Go to the page <a href="https://mitchdigital.online/">MitchDigital</a></p>`)
 })
+
+
+
 
 
 app.listen(puerto,()=>{
     console.log("App listen port "+puerto);
 })
+*/
--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,5 +1,7 @@
 console.log("Hola");
-$('#nav__link__sub').hover(function() {
+
+///// CONTENIDO DE PRUEBA
+/*$('#nav__link__sub').hover(function() {
     $('.nav-items-1').toggleClass('navActive')
   })
 console.log("Hola");
@@ -13,5 +15,54 @@ buttonMenu.addEventListener('click', (e) => {
   navMenu.classList.toggle('open-menu');
 });
 
+
+*/
+
+// FUNCION DE CARGUE PARA SECTIONS
+
+function loadSection(event, section){ 
+  console.log(`HolaFunction en seccion: ${section}`);
+  //evitar cambiar url o recargar la página
+  event.preventDefault();
+  
+  //Realizar la peticion al servidor para obtener el contenido
+  fetch(`/section/${section}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Sección no encontrada');
+      }
+      return response.text();
+    })
+    .then(data => {
+      console.log("Esta buscando el inner");
+      document.getElementById('dynamicSection').innerHTML = data;
+    })
+    .catch(error => console.error('Error al cargar la seccion: ',error));
+}
+
+
+// FUNCION PARA MENU RESPONSIVE
+
+document.addEventListener("DOMContentLoaded", () =>{
+  const toogleButton = document.querySelector(".navbar__toogle-btn");
+  const mobileMenu = document.querySelector(".navbar__list-mobile");
+
+  // si el menu esta oculto o vacio, lo muestre cambiando a flex, si esta visible lo cambie a none
+  const toogleMenu = () => {
+    mobileMenu.style.display =
+      mobileMenu.style.display === "none" || mobileMenu.style.display === ""
+      ? "flex"
+      : "none";
+  };
+
+  const hideMenuResize = () => {
+    mobileMenu.style.display = "none"
+  }
+
+  toogleButton.addEventListener("click",toogleMenu);
+  window.addEventListener("resize",hideMenuResize);
+  window.addEventListener("load",hideMenuResize);
+
+})
 
 
